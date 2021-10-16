ad743ec94b74b25b69e0c38168df28661d1bd2ac
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-16 17:59:33 +0300

    formatting

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-// Environments and essential file
+/****************************************************
+ *     APPLICATION ENVIRONMENTS AND CONSTANTS       *
+ *                  DEFINITION                      *
+ *****************************************************/
 use core\Route;
 use duncan3dc\Laravel\Blade;
 
@@ -28,40 +31,40 @@ Blade::addPath(views.'/errors');
 Blade::addPath(views.'/public');
 define('layout', views.'/layout');
 
-// If your script lives in a subfolder you can use the following example
-// Do not forget to edit the basepath in .htaccess if you are on apache
-// define('BASEPATH','/api/v1');
-// Add base route (startpage)
+
+/****************************************************
+ *              YOU CAN DEFINE YOUR ROUTES          *
+ *                  STARTING FROM HERE              *
+ ****************************************************/
+
 Route::add('/', function() {
-    return Blade::render("welcome", ['host'=>config('db_host')]);
+    return Blade::render("welcome");
 });
 
 Route::add('/test', function() {
-    //
+    echo 'hi there, welcome to the DyF family';
 });
 
-// 405 test
-Route::add('/this-route-is-defined', function() {
-    echo 'You need to patch this route to see this content';
-}, 'patch');
 
-// Add a 404 not found route
+/****************************************************
+ *                ERROR PAGES 404, 405              *
+ *             DO NOT CHANGE THIS SECTION           *
+ ****************************************************/
+
 Route::pathNotFound(function($path) {
     echo Blade::render("404", ['path' => $path]);
 });
 
-// Add a 405 method not allowed route
 Route::methodNotAllowed(function($path, $method) {
-    // Do not forget to send a status header back to the client
-    // The router will not send any headers by default
-    // So you will have the full flexibility to handle this case
-    header('HTTP/1.0 405 Method Not Allowed');
-    return Blade::render("error.405");
-    echo 'The requested path "'.$path.'" exists. But the request method "'.$method.'" is not allowed on this path!';
+    echo Blade::render("405", ['method' => $method]);
 });
 
-// Return all known routes
-Route::add('/known-routes', function() {
+
+/****************************************************
+ *      VIEW REGISTERED ROUTES AND THEIR METHODS    *
+ *                  USE THIS SECTION                *
+ ****************************************************/ 
+Route::add('/routes', function() {
     $routes = Route::getAll();
     echo '<ul>';
     foreach ($routes as $route) {
