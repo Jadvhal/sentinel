c4954ced10619f4ff8bd7351128ca3d051b0baa9
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-16 08:30:35 +0300

    initial commit

---
+++ controls/auth/authorize.php
@@ -0,0 +1,24 @@
+<?php
+namespace control;
+
+class Authorize{
+    //register user
+    public static function register(){
+
+    }
+
+    //log the user
+    public static function login(){
+
+    }
+
+    //reset user credentials
+    public static function reset(){
+
+    }
+
+    //verify user session
+    public static function verify(){
+
+    }
+}
---
+++ controls/inmate/inmate.php
@@ -0,0 +1,14 @@
+<?php
+namespace control;
+
+class inmate{
+    //list inmates
+    public static function list(){
+
+    }
+
+    //view specific inmate
+    public static function view(){
+        
+    }
+}
