f7000e84d279a6d38cbe47c7ace98eeb246bfbdb
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-16 13:02:51 +0300

    load db data from .env

--- core/Configuration.php
+++ core/Configuration.php
@@ -3,32 +3,17 @@ namespace core;
 
 class Database{
 	public static function conn(){
-		$servername = "localhost";
-		$username   = "root";
-		$password   = "";
-		$database   = "mani";
+		$servername = config('db_host');
+		$username   = config('db_user');
+		$password   = config('db_pass');
+		$database   = config('db_data');
 		$conn = new mysqli($servername, $username, $password, $database);
 		if ($conn->connect_error) {
+			include 'views/errors/db.html';
 			die("Connection failed: " . $conn->connect_error);
 		}
 		
 		return $conn;
-	}
-}
-
-class Site{
-	public static  function configs(){
-		$conn  = Database::conn();
-		$query = "SELECT key, value FROM config";
-		$query = $conn->query($query);
-
-		if ($query->num_rows > 0) {
-			$config = $query->fetch_all(MYSQLI_ASSOC);
-		} else {
-			$config = 0;
-		}
-
-		return $config;
 	}
 }
 ?>
