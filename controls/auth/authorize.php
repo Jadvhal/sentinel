6eba01f48f21f2fcb79f2838ae39d1454465826a
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-17 11:29:52 +0300

    register

--- controls/auth/authorize.php
+++ controls/auth/authorize.php
@@ -1,24 +1,38 @@
 <?php
 namespace control;
 
+use mysqli;
+
 class Authorize{
-    //register user
-    public static function register(){
+	//register user
+	public static function register(){
+		$user   = mysqli_real_escape_string(conn(), $_POST['username']);
+		$email  = mysqli_real_escape_string(conn(), $_POST['email']);
+		$pass	= mysqli_real_escape_string(conn(), $_POST['pass']);
 
-    }
+		$sql = "insert into user (username, email, password) values ('$user', '$email', '$pass')";
 
-    //log the user
-    public static function login(){
+		if(mysqli_query(conn(), $sql)){
+			setcookie('notice', 'df_reg_200', time() + (86400 * 30), "/"); 
+		}else{
+			setcookie('notice', 'df_reg_201', time() + (86400 * 30), "/");
+		}
 
-    }
+		header('Location: /notice/register');
+	}
 
-    //reset user credentials
-    public static function reset(){
+	//log the user
+	public static function login(){
 
-    }
+	}
 
-    //verify user session
-    public static function verify(){
+	//reset user credentials
+	public static function reset(){
 
-    }
+	}
+
+	//verify user session
+	public static function verify(){
+
+	}
 }
