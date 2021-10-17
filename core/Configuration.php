5b201cc221b1a74b5a876d05d0c133ce289c8372
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-17 11:30:23 +0300

    fetch cred using env global variable

--- core/Configuration.php
+++ core/Configuration.php
@@ -1,19 +1,17 @@
 <?php
-namespace core;
 
-class Database{
-	public static function conn(){
-		$servername = config('db_host');
-		$username   = config('db_user');
-		$password   = config('db_pass');
-		$database   = config('db_data');
-		$conn = new mysqli($servername, $username, $password, $database);
-		if ($conn->connect_error) {
-			include 'views/errors/db.html';
-			die("Connection failed: " . $conn->connect_error);
-		}
-		
-		return $conn;
+function conn(){
+	$servername = $_ENV['DB_HOST'];
+	$username   = $_ENV['DB_USER'];
+	$password   = $_ENV['DB_PASS'];
+	$database   = $_ENV['DB_DATA'];
+	$conn = new mysqli($servername, $username, $password, $database);
+	if ($conn->connect_error) {
+		include 'views/errors/db.html';
+		die("Connection failed: ");
 	}
+	
+	return $conn;
 }
+
 ?>
