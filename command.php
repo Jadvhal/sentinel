01cf6dddf3cb018f80ff743eeff817d2e4195e89
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-18 22:00:07 +0300

    command

--- command.php
+++ command.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Console;
-include 'vendor/autoload.php';
+
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
