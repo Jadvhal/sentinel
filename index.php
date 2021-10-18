ca54f9e34a882278204e87f4b022fa1677a5445c
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-18 18:28:51 +0300

    authentication, session

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  *     ERROR HANDLER, HANDLES ALL SCRIPT ERRORS      *
  *             EXCEPT SYNTAX ERRORS                  *
  *****************************************************/
-
+session_start();
 include 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '.env');
 $dotenv->load();
@@ -23,8 +23,10 @@ $whoops->register();
  *       APPLICATION ENVIRONMENTS AND CONSTANTS      *
  *****************************************************/
 
+use control\Authorize;
 use core\Route;
 use duncan3dc\Laravel\Blade;
+use Illuminate\Support\Facades\Auth;
 
 include 'core/Route.php';
 include 'core/Loader.php';
@@ -44,7 +46,7 @@ define('cache', 	'cache');
 // Define views global paths
 Blade::addPath(views.'/auth');
 Blade::addPath(views.'/errors');
-
+Blade::addPath(views.'/public');
 
 /****************************************************
  *              YOU CAN DEFINE YOUR ROUTES          *
@@ -55,8 +57,8 @@ Route::add('/', function() {
     return Blade::render("welcome");
 });
 
-Route::add('/test', function() {
-    echo "hello from DyF";
+Route::add('/home', function() {
+    return Blade::render('home');
 });
 
 
@@ -64,8 +66,15 @@ Route::add('/test', function() {
  *                 AUTHENTICATICATION               *
  *             DO NOT CHANGE THIS SECTION           *
  ****************************************************/
-Route::add('/authorize', function() {
-    return Blade::render("welcome");
+Route::add('/authorize/([a-z]*)', function($page) {
+    switch ($page){
+        case 'register' : Authorize::register(); break;
+        case 'login'    : Authorize::login(); break;
+        case 'reset'    : break;
+        case 'verify'   : break;
+        default:
+            header('Location: /404');
+    }
 }, 'post');
 
 Route::add('/login', function() {
@@ -80,8 +89,8 @@ Route::add('/reset', function() {
     return Blade::render("reset");
 });
 
-Route::add('/notice/([A-Za-z])*', function($page) {
-    return Blade::render("notice");
+Route::add('/notice/([a-z]*)', function($page) {
+    return Blade::render("notice", ['page'=>$page]);
 });
 
 /****************************************************
