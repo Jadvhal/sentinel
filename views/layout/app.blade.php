686c1e263148a7e3bf4523cccc18a36d2bb3a1f3
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-18 18:32:18 +0300

    main header template

--- views/layout/app.blade.php
+++ views/layout/app.blade.php
@@ -1,35 +1,68 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 	<head>
 		<!-- please do not remove this section -->
 		<meta charset="utf-8">
 		<meta content="width=device-width, initial-scale=1.0" name="viewport">
 		<meta name="generator" content="Dynamic Framework">
 		<title>@yield('title')</title>
+		<meta content="{{$_ENV['AUTHOR']}}" name="description">
+		<meta name="description" content="{{$_ENV['DESCRIPTION']}}">
 		<!-- please do not remove this section -->
-		<meta content="Dynamic Framework, A php starter kit you must have" name="description">
-		<meta content="dynamic, framework, php, blade template, imap, smtp, smpp" name="keywords">
+
 		<!-- Favicons -->
 		<link href="/assets/img/favicon.png" rel="icon">
 		<link href="/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 		<!-- Google Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Roboto:100,300,400,500,700|Philosopher:400,400i,700,700i" rel="stylesheet">
-		<!-- Vendor CSS Files -->
-		@css("/assets/vendor/aos/aos.css")
-		@css("/assets/vendor/bootstrap/css/bootstrap.min")
-		@css("/assets/vendor/bootstrap-icons/bootstrap-icons")
-		@css("/assets/vendor/glightbox/css/glightbox.min")
-		@css("/assets/vendor/swiper/swiper-bundle.min")
-		@css("/assets/css/style")
+		<style type="text/css">
+			body {
+			background-color: #eee;
+			}
+			body, h1, p {
+			font-family: "Helvetica Neue", "Segoe UI", Segoe, Helvetica, Arial, "Lucida Grande", sans-serif;
+			font-weight: normal;
+			margin: 0;
+			padding: 0;
+			text-align: center;
+			}
+			.container {
+			margin-left:  auto;
+			margin-right:  auto;
+			margin-top: 177px;
+			max-width: 1170px;
+			padding-right: 15px;
+			padding-left: 15px;
+			}
+			.row:before, .row:after {
+			display: table;
+			content: " ";
+			}
+			.col-md-6 {
+			width: 50%;
+			}
+			.col-md-push-3 {
+			margin-left: 25%;
+			}
+			h1 {
+			font-size: 48px;
+			font-weight: 300;
+			margin: 0 0 20px 0;
+			}
+			.lead {
+			font-size: 21px;
+			font-weight: 200;
+			margin-bottom: 20px;
+			}
+			p {
+			margin: 0 0 10px;
+			}
+			a {
+			color: #3282e6;
+			text-decoration: none;
+			}
+		</style>
 	</head>
 	<body>
 		@yield('content')
-		<!-- Vendor JS Files -->
-		@js('/assets/vendor/aos/aos')
-		@js('/assets/vendor/bootstrap/js/bootstrap.bundle.min')
-		@js('/assets/vendor/glightbox/js/glightbox.min')
-		@js('/assets/vendor/php-email-form/validate')
-		@js('/assets/vendor/swiper/swiper-bundle.min')
-		@js('/assets/js/main')
 	</body>
 </html>
