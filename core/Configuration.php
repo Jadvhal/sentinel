c4a3a7492905ee5ae383cf642d268d51f79daa64
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-18 18:30:04 +0300

    removed garbage code

--- core/Configuration.php
+++ core/Configuration.php
@@ -5,12 +5,8 @@ function conn(){
 	$username   = $_ENV['DB_USER'];
 	$password   = $_ENV['DB_PASS'];
 	$database   = $_ENV['DB_DATA'];
+
 	$conn = new mysqli($servername, $username, $password, $database);
-	if ($conn->connect_error) {
-		include 'views/errors/db.html';
-		die("Connection failed: ");
-	}
-	
 	return $conn;
 }
 
