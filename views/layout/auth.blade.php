f6d22f4f4dc9e448acebfdf2f0252e15286a6ddd
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-18 18:32:46 +0300

    authorization header layout

--- views/layout/auth.blade.php
+++ views/layout/auth.blade.php
@@ -5,8 +5,9 @@
 		<meta content="width=device-width, initial-scale=1.0" name="viewport">
 		<meta name="generator" content="Dynamic Framework">
 		<title>@yield('title')</title>
-		<meta name="description" content="Authentication forms">
-		<meta name="author" content="Arasari Studio">        
+		<meta name="author" content="{{$_ENV['AUTHOR']}}"> 
+		<meta name="description" content="{{$_ENV['DESCRIPTION']}}">
+		<link rel="icon" href="/assets/img/favicon.png">     
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&amp;display=swap" rel="stylesheet">
         @css('/assets/auth/css/bootstrap.min')
         @css('/assets/auth/css/common')
