e7bf04df9eb495a6a89b6691fe9bbd1edef8d82a
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-18 18:29:32 +0300

    login, registrations

--- controls/auth/authorize.php
+++ controls/auth/authorize.php
@@ -6,23 +6,49 @@ use mysqli;
 class Authorize{
 	//register user
 	public static function register(){
-		$user   = mysqli_real_escape_string(conn(), $_POST['username']);
+		$user   = mysqli_real_escape_string(conn(), $_POST['user']);
 		$email  = mysqli_real_escape_string(conn(), $_POST['email']);
-		$pass	= mysqli_real_escape_string(conn(), $_POST['pass']);
+		$pass	= md5(mysqli_real_escape_string(conn(), $_POST['pass']));
 
-		$sql = "insert into user (username, email, password) values ('$user', '$email', '$pass')";
+		$check = "select username, user_email from users where username = '$user' or user_email = '$email'";
+		$check = mysqli_query(conn(), $check);
 
-		if(mysqli_query(conn(), $sql)){
-			setcookie('notice', 'df_reg_200', time() + (86400 * 30), "/"); 
+		if(mysqli_num_rows($check) > 0){
+			$data = mysqli_fetch_assoc($check);
+			if($data['username'] == $user){
+				setcookie('notice', 'df_reg_201', time() + (86400 * 30), "/"); 
+			}else{
+				setcookie('notice', 'df_reg_202', time() + (86400 * 30), "/"); 
+			}
 		}else{
-			setcookie('notice', 'df_reg_201', time() + (86400 * 30), "/");
+			$query = "insert into users (username, user_email, user_pass) values ('$user', '$email', '$pass')";
+			if(mysqli_query(conn(), $query)){
+				setcookie('notice', 'df_reg_200', time() + (86400 * 30), "/"); 
+			}else{
+				setcookie('notice', 'df_reg_209', time() + (86400 * 30), "/");
+			}
 		}
-
+		
 		header('Location: /notice/register');
 	}
 
 	//log the user
 	public static function login(){
+		$user = mysqli_real_escape_string(conn(), $_POST['email']);
+		$pass = md5(mysqli_real_escape_string(conn(), $_POST['password']));
+
+		$sql =  "select user_pass from users where username = '$user' or user_email = '$user'";
+		if(mysqli_num_rows(mysqli_query(conn(), $sql)) > 0){
+			$data = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+			if($data['user_pass'] == $pass){
+				Session::init($user);
+				setcookie('notice', 'df_log_200', time() + (86400 * 30), "/");
+			}
+		}else{
+			setcookie('notice', 'df_log_201', time() + (86400 * 30), "/");
+		}
+
+		header('Location: /notice/auth');
 
 	}
 
