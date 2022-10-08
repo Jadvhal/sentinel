1f05cd7913e48be4737d4bc1a56d0601398c5e8b
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-10-08 21:40:49 +0300

    minor layout fix

--- views/layout/auth.blade.php
+++ views/layout/auth.blade.php
@@ -9,16 +9,16 @@
 		<meta name="description" content="{{$_ENV['DESCRIPTION']}}">
 		<link rel="icon" href="/assets/img/favicon.png">     
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&amp;display=swap" rel="stylesheet">
-        @css('/assets/auth/css/bootstrap.min')
-        @css('/assets/auth/css/common')
-        @css('/assets/auth/css/auth')
+		@css('/assets/auth/css/bootstrap.min')
+		@css('/assets/auth/css/common')
+		@css('/assets/auth/css/auth')
 	</head>
 	<body>
 
-        @yield('content')
-        @js('/assets/auth/js/jquery.min')
-        @js('/assets/auth/js/bootstrap.min')
-        @js('/assets/auth/js/main')
+		@yield('content')
+		@js('/assets/auth/js/jquery.min')
+		@js('/assets/auth/js/bootstrap.min')
+		@js('/assets/auth/js/main')
 
 	</body>
 </html>
