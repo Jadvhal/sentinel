9e7190865a8e29c9d11f57af7af2ca7487735fd6
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-12-16 10:20:28 +0300

    moved loader fns to engine, improved __()

--- core/Loader.php
+++ core/Loader.php
@@ -1,34 +1,5 @@
 <?php
-# class loader
-function load_controllers($directory): void {
-	if(is_dir($directory)) :
-		$scan = scandir($directory);
-		unset($scan[0], $scan[1]); # unset . and ..
 
-		# foreach scaned directory recall the function
-		foreach($scan as $file):
-			if(is_dir($directory."/".$file)):
-				load_controllers($directory."/".$file);
-			else:
-				if(strpos($file, '.php') !== false):
-					require_once($directory."/".$file);
-				endif;
-			endif;
-		endforeach;
-	endif;
-}
-
-
-# language and localization function
-function __($key){
-	$lang = isset($_COOKIE['lang']) ? $_COOKIE['lang'].'.json' : 'en.json';
-	$term = file_get_contents(locale . '/' . $lang);
-	$term = json_decode($term, true);
-
-	return $term[$key];
-}
-
-# load the systems core files
 require_once 'engine.php';
 load_controllers('controls');
 ?>
--- core/engine.php
+++ core/engine.php
@@ -14,4 +14,32 @@ function db(){
 	);
 }
 
+
+# classes loader
+function load_controllers($directory): void {
+	if(is_dir($directory)) :
+		$scan = scandir($directory);
+		unset($scan[0], $scan[1]); # unset . and ..
+
+		# foreach scaned directory recall the function
+		foreach($scan as $file):
+			if(is_dir($directory."/".$file)):
+				load_controllers($directory."/".$file);
+			else:
+				if(strpos($file, '.php') !== false):
+					require_once($directory."/".$file);
+				endif;
+			endif;
+		endforeach;
+	endif;
+}
+
+
+# language and localization function
+function __($key){
+	$lang = $_COOKIE['lang'] ?? 'en';
+	$term = file_get_contents(locale . "/$lang.json");
+	return json_decode($term)->$key;
+}
+
 ?>
