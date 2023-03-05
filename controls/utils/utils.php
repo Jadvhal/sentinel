60c38cf5f7f087ef594e7a2492c6d791d21958fd
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2023-03-06 00:02:41 +0300

    add upload file format checker (not tested)

--- controls/utils/utils.php
+++ controls/utils/utils.php
@@ -1,29 +1,33 @@
 <?php
 class Utils{
-    public static function upload($file='media', $dir=''){
+    /**
+     * @param $file - request input name
+     * @param $dir - storage path in upload directory
+     * @param null $type - takes in string, array, or null
+     * @return string
+     */
+    public static function upload($file='file', $dir='', $type=null){
         $target_dir = '/uploads//' . $dir;
         $imageFileType = strtolower(pathinfo($_FILES[$file]["name"],PATHINFO_EXTENSION));
         $_FILES["media"]['name'] = round(microtime(true)) . rand(1111, 99999) . '.' . $imageFileType;
         $target_file = $target_dir . basename($_FILES["media"]["name"]); $uploadOk = 1;
         
-        //use this sample to set which files to upload
-        /*if(isset($target_dir)) {
-            if($_FILES["media"]['type']=='image/'.$imageFileType or $_FILES["media"]['type']=='video/'.$imageFileType or $_FILES["media"]['type']=='image/jpeg') {
-                $uploadOk = 1;
-            } else {
-                $uploadOk = 0;
-            }
-        }*/
+        // check the submitted file type
+        if($type != null):
+            if(is_array($type)): if(in_array($imageFileType, $type)): $uploadOk = 1; else: $uploadOk = 0; endif;
+                else: if($imageFileType == $type): $uploadOk = 1; else:  $uploadOk = 0;  endif;
+            endif;
+        endif;
         
-        if ($uploadOk == 0) {
+        if ($uploadOk == 0) :
             echo "Wrong media format.";
-        } else {
-            if (move_uploaded_file($_FILES[$file]["tmp_name"], $target_file)) {
+        else :
+            if (move_uploaded_file($_FILES[$file]["tmp_name"], $target_file)) :
                 return $target_file;
-            } else {
+            else :
                 echo "Sorry, there was an error uploading your file.";
-            }
-        }
+            endif;
+        endif;
     }
 }
 ?>
