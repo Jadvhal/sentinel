cf1be5b33d1a4c8f8fb76956b455619d7ebf0f68
ibnsultan <abdulbasitsultan4@gmail.com>
2023-05-26 14:40:02 +0300

    null safety for localization, code cleansing

--- core/engine.php
+++ core/engine.php
@@ -1,6 +1,7 @@
 <?php
 
 use Nette\Utils\Html;
+use duncan3dc\Laravel\Blade;
 use Nette\Database\Connection;
 
 function db(){
@@ -36,23 +37,59 @@ function load_controllers($directory): void {
 	endif;
 }
 
-
-# language and localization function
-function __($key){
-	$lang = $_COOKIE['lang'] ?? 'en';
-	$term = file_get_contents(locale . "/$lang.json");
-	return json_decode($term)->$key;
-}
-
 # config function
 function config($key){
 	$key = strtoupper($key);
 	return $_ENV[$key];
 }
 
-# make the HTML globally available
-function render(){
-	return new Html();
+# HTML render and view template engine
+function render(){ return new Html(); }
+function view($path, $data = []){ return Blade::render($path, $data);}
+
+function viewPaths($base = views, $paths = []){
+	foreach($paths as $path) Blade::addPath("$base/$path");
 }
+
+/***************************************
+ * Language and localization functions *
+ **************************************/
+
+# language and localization function
+function __($key){
+	$lang = isset($_COOKIE['lang']) ? $_COOKIE['lang'].'.json' : 'en.json';
+	$term = file_get_contents(locale . '/' . $lang);
+	$term = json_decode($term, true);
+
+	return $term[$key] ?? $key;
+}
+
+// this function is used by the ___() function
+function spin($foo, $bar){	
+	if(isset($_SESSION['id'])): return $bar;
+		else: return $foo;
+	endif;
+}
+
+// return output depending on the user's login status
+function ___($foo, $bar){
+	$key = spin($foo, $bar);
+	return __($key);
+}
+
+// content iterator function for localization
+function __e($key, $el){
+	# el sample : li, h1, h2, h3, h4, h5, h6, p, span, a, div, etc
+	# key sample : "feature_list" : [  "ብቃት ያላቸው አስተማሪዎች",  "ነጻ እና መስተጋብራዊ ትምህርት",  "AI-Powered ግምገማ" ]
+
+	$key = __($key); $res = '';
+	foreach ($key as $value)  $res .= "<$el>$value</$el>";
+	
+	return $res;
+}
+
+/*******************************************
+ * End Language and localization functions *
+ ******************************************/
 
 ?>
